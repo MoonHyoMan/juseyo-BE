e7abafe6c058055ef7cadcd345877e28ee06fa59
Doyeon <duseh729@naver.com>
2024-11-06 05:40:10 +0900

    feat: 로그인 후 토큰 발급 기능 구현 postman Auth에 토큰이 있어서 계속해서 오류가 있었다,,

--- src/main/java/com/moonhyoman/juseyo_be/controller/UserController.java
+++ src/main/java/com/moonhyoman/juseyo_be/controller/UserController.java
@@ -2,7 +2,10 @@ package com.moonhyoman.juseyo_be.controller;
 
 import com.moonhyoman.juseyo_be.domain.User;
 import com.moonhyoman.juseyo_be.dto.LoginRequest;
+import com.moonhyoman.juseyo_be.security.jwt.JwtGenerator;
+import com.moonhyoman.juseyo_be.security.jwt.JwtToken;
 import com.moonhyoman.juseyo_be.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,23 +13,30 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
-
     private final UserService userService;
+    private final JwtGenerator jwtGenerator;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, JwtGenerator jwtGenerator) {
         this.userService = userService;
+        this.jwtGenerator = jwtGenerator;
     }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequest LoginRequest) {
         User user = userService.login(LoginRequest);
+        log.info("user: {}", user.getId());
         if(user == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("로그인 실패");
         }
-        return ResponseEntity.ok(user);
+        // JWT 생성
+        JwtToken jwt = jwtGenerator.generateToken(user.getId());
+
+        // JWT를 포함한 응답
+        return ResponseEntity.ok(jwt);
     }
 }
