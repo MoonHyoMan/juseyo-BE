9d06264f16f34d904988455c372ca2de7c80e75b
Doyeon <duseh729@naver.com>
2024-11-06 05:49:24 +0900

    fix: 인증 로직은 잘 되는데 에러 뜨는 거 JwtAuthenticationFilter>chain.doFilter(request, response) 성공 시에는 필요 없는 듯 함

--- src/main/java/com/moonhyoman/juseyo_be/controller/UserController.java
+++ src/main/java/com/moonhyoman/juseyo_be/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -38,5 +39,12 @@ public class UserController {
 
         // JWT를 포함한 응답
         return ResponseEntity.ok(jwt);
+    }
+
+    @GetMapping("/token-test")
+    public String testToken(Authentication authentication) {
+        String id = authentication.getName();
+        System.out.println(id);
+        return id;
     }
 }
