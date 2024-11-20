237a33bc9a707afebb083fba76446abd980af61e
문준일 <answnsdlfwkd@naver.com>
2024-11-21 06:38:08 +0900

    feat: 내 정보 조회 추가

--- src/main/java/com/moonhyoman/juseyo_be/service/UserService.java
+++ src/main/java/com/moonhyoman/juseyo_be/service/UserService.java
@@ -1,9 +1,31 @@
 package com.moonhyoman.juseyo_be.service;
 
-import lombok.extern.slf4j.Slf4j;
+import com.moonhyoman.juseyo_be.domain.User;
+import com.moonhyoman.juseyo_be.dto.UserResponse;
+import com.moonhyoman.juseyo_be.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserResponse getMyInfo(String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .password(user.getPassword()) // 보안을 위해 필요에 따라 암호화 처리 가능
+                .accountNum(user.getAccountNum())
+                .type(user.getType())
+                .build();
+    }
 }
